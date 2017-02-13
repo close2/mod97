daed97f2f39ac771319a7042a0415f01d00b9965
Christian Loitsch <christian@loitsch.com>
2017-02-13 22:11:19 +0100

    fix: spelling

--- example/mod97_example.dart
+++ example/mod97_example.dart
@@ -3,6 +3,6 @@ import 'package:mod97/mod97.dart';
 int main() {
   final hugeNumber = '12345678901234567890';
   int mod = mod97(hugeNumber);
-  print('The modulos 97 of $hugeNumber is: $mod');
+  print('The modulo 97 of $hugeNumber is: $mod');
   return 0;
 }
